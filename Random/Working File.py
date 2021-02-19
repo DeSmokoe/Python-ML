bf3b52b726eceb91aad9f10d9dd97273d99d2c9e
Alex Van Dromme <s0182579@ad.ua.ac.be>
2021-02-19 23:08:42 +0100

    Initial commit

---
+++ Random/Working File.py
@@ -0,0 +1,18 @@
+def keuze(input):
+    if input.upper() == "ALEX":
+        print("Fok joe")
+    else:
+        print(input.upper())
+
+
+# keuze(input("Piemel: "))
+
+number = 0
+
+for i in range(5):
+    number = number + 1
+    if number == 2:
+        continue
+    if number == 4:
+        break
+    print(number)
